cf1088ac3831f756feddda95d0e05ee5e86026f5
Ahmedi180 <128325364+Ahmedi180@users.noreply.github.com>
2026-03-14 10:09:48 +0500

    Update app.js

--- app.js
+++ app.js
@@ -116,7 +116,7 @@ if (/-E\s*FORM/i.test(company) || /-EFORM/i.test(company)) return false;
 if (value >= 500) return false;
 
 // Sialkot city filter
-if (!(city.includes("SIALKOT") || city.includes("SKT") || city.includes("SKTA"))) return false;
+if (city.includes("SIALKOT") || city.includes("SKT")) { return true;
   
 // lahore ko force reject
 if (city.includes("LAHORE")) return false;
